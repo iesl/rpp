d1610bbcd8adc10d04d5cf59702b1cc5d88ba866
k8si <ksilvers@umass.edu>
2015-03-31 18:30:12 -0400

    debug

--- src/main/scala/edu/umass/cs/iesl/rpp/Main.scala
+++ src/main/scala/edu/umass/cs/iesl/rpp/Main.scala
@@ -106,6 +106,14 @@ object Main {
     val refTags = getAllAnnotationTypes(annotator).filter(t => t.startsWith("ref-"))
     val refTokenBIndexPairSet = annotator.getBIndexPairSet(Single(SegmentCon("reference-token")))
     val allAnnots = new scala.collection.mutable.ArrayBuffer[List[(String, String)]]()
+    refTokenBIndexPairSet.toList.foreach {
+      case (bi, ci) =>
+        val annots = new scala.collection.mutable.ArrayBuffer[(String, String)]()
+        val title = annotator.getTextMap("ref-title")(bi, ci).values.map(_._2).mkString("")
+        val titlePair: (String, String) = ("ref-title", title)
+        annots += titlePair
+        allAnnots += annots.toList
+    }
 //    refTokenBIndexPairSet.toList.foreach {
 //      case (bi, ci) =>
 //        val annots = new scala.collection.mutable.ArrayBuffer[(String, String)]()
